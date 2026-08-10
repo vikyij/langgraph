476a71f2fc531d32982c95f3b68da03abfc6d1da
vikyij <vikkyij54@gmail.com>
2026-08-10 10:58:10 -0400

    learn different state schemas - typeddict, dataclass, pydantic

---
+++ module-2/state-schema.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, field_validator, ValidationError
+
+class PydanticState(BaseModel):
+    name: str
+    mood: str # "happy" or "sad" 
+
+    @field_validator('mood')
+    @classmethod
+    def validate_mood(cls, value):
+        # Ensure the mood is either "happy" or "sad"
+        if value not in ["happy", "sad"]:
+            raise ValueError("Each mood must be either 'happy' or 'sad'")
+        return value
+
+try:
+    state = PydanticState(name="John Doe", mood="mad")
+except ValidationError as e:
+    print("Validation Error:", e)
